0f08c79e22b6945aa9b3d92e726e2ef1eb3a38fc
Nimie8203 <nimie8203@proton.me>
2026-04-18 16:37:47 +0300

    Flow sens beginnnings

--- embedded/include/flow_sensor.h
+++ embedded/include/flow_sensor.h
@@ -6,4 +6,15 @@
 #define FLOW_SENS_1_PIN 22
 #define FLOW_SENS_2_PIN 23
 
+#define PULSES_PER_LITER 50
+#define SAMPLE_PERIOD 1000  // UNIT MILLISECONDS
+
+typedef enum {
+    FS_1,
+    FS_2
+} FLOW_SENS_NUM;
+
+void init_flow_sensors(void);
+void read_flow_sens(void);
+
 #endif
